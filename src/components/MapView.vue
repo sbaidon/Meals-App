f7feae51305d0bf316ef2f983b6636a2d0569104
Sergio Emir Baidon Carrillo <sbaidon@outlook.com>
2017-08-07 08:46:30 -0500

    Implement google maps API, create MapView and Modal Components

---
+++ src/components/MapView.vue
@@ -0,0 +1,147 @@
+<template>
+  <div class="columns is-multiline">
+    <div class="field column is-12">
+      <p class="control has-icons-left has-icons-right">
+        <input id="autocomplete" class="input" type="text" placeholder="Search">
+        <span class="icon is-small is-right">
+          <i class="fa fa-search"></i>
+        </span>
+      </p>
+    </div> 
+    <div class="column is-12">
+      <div id="map"></div>
+    </div>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'map-view',
+  data () {
+    return {
+      lat: 51.50,
+      long: -0.127,
+      selectedPlace: {}
+    }
+  },
+  computed: {
+    mapContainer() {
+      return this.$el.querySelector('#map')
+    },
+    input() {
+      return this.$el.querySelector('#autocomplete')
+    },
+    location() {
+      return new google.maps.LatLng(this.lat, this.long);
+    },
+    mapOptions() {
+      return {
+        center: this.location,
+        scrollwheel: false,
+        zoom: 15
+      }
+    },
+    nearbyOptions() {
+      return {
+        location: this.location,
+        radius: '500',
+        type: ['restaurant']
+      }
+    },
+    autocompleteOptions() {
+      return {
+        types: ['establishment']
+      }
+    }
+  },
+  mounted() {
+    this.getLocation()
+    this.attachMap()
+    this.addAutocomplete()
+  },
+  methods: {
+    addAutocomplete() {
+      this.matches = new google.maps.places.Autocomplete(this.input, this.autocompleteOptions);
+      this.matches.addListener('place_changed', () => {
+        this.removeMarker()
+        this.selectedPlace = this.matches.getPlace()
+        this.createMarker(this.selectedPlace)
+      })
+    },
+    getLocation() {
+      if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(this.receivePosition, this.handleError)
+        return
+      }
+    },
+    removeMarker() {
+      if (this.selectedPlace) {
+        
+      }
+    },
+    createMarker(place) {
+      const lat = place.geometry.location.lat() 
+      const lng = place.geometry.location.lng()
+      const location = new google.maps.LatLng(lat, lng)
+      const marker = new google.maps.Marker({
+        position: location, 
+        title: place.name,
+      });
+      marker.setMap(this.map)
+      this.map.setCenter({ lat, lng }); 
+      this.addInfoWindow(marker, place)
+    },
+    addInfoWindow(marker, place) {
+      const infoWindow = new google.maps.InfoWindow()
+      marker.addListener('click', () => {
+        infoWindow.setContent(this.getInfoWindowHtml(place));
+        infoWindow.open(this.map, marker);
+      }) 
+    },
+    getInfoWindowHtml(place) {
+      let image = ''
+      if (place.photos.length) {
+        image = place.photos[0].getUrl({
+          maxWidth: 248 
+        })
+      }
+      return `
+        <div class="popup">
+            <figure class="image is-128x128">
+              <img src="${image || ''}" alt="${place.name}"> 
+            </figure>
+            <p>${place.name}</p>
+        </div>
+      `;  
+    },
+    getNearbyPlaces() {
+      this.service = new google.maps.places.PlacesService(this.map);
+      this.service.nearbySearch(this.nearbyOptions, (results, status) => {
+        if (status == google.maps.places.PlacesServiceStatus.OK) {
+          results.forEach(place => this.createMarker(place))
+        }
+      });
+    },
+    handleError(error) {
+      console.log(error)
+    },
+    receivePosition(position) {
+      this.lat = position.position.coords.latitude
+      this.long = position.coords.longitde
+    },
+    attachMap() {
+      this.map = new google.maps.Map(this.mapContainer, this.mapOptions)
+    },
+    getNearbyRestaurants() {
+      this.$store.dispatch('getNearbyRestaurants')
+    }
+  }
+}
+</script>
+
+<style lang='sass'>
+  #map 
+    height: 300px
+    width: 100%
+</style>
---
+++ src/components/Modal.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="modal" :class="{ 'is-active': isOpen }">
+    <div class="modal-background" @click="closeModal"></div>
+    <div class="modal-content">
+      <slot></slot> 
+    </div>
+    <button class="modal-close is-large" @click="closeModal"></button>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'modal',
+  props: ['isOpen'],
+  data() {
+      return {
+
+      }
+  },
+  methods: {
+    closeModal() {
+        this.$emit('closeModal')
+    }
+  }
+}
+</script>
+
+<style>
+  
+</style>
