8121f35ff2c9490527f03f87aafeeca577da3e0a
Sergio Emir Baidon Carrillo <sbaidon@outlook.com>
2017-08-07 21:21:44 -0500

    Simplify order creation, retrieve restaurant's image.

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -8,33 +8,16 @@
       </div>
     </div>
     <tabs :tabs="tabs"></tabs>
-    <div class="columns is-multiline">
-      <div class="column is-narrow">
-        <a class="button is-warning" @click="isModalOpen = true">Add a new order</a>
-      </div>
-      <div clas="column is-12" v-for="(order, index) in allOrders" :key="index">
-        <p>{{ order }}</p>
-      </div>
+    <div class="field">
+      <a class="button is-warning" @click="isModalOpen = true">Add a new order</a>
     </div>
-    <modal @closeModal="isModalOpen = false" v-if="isModalOpen">
+    <router-view class="section"></router-view>
+    <modal @closeModal="handleCloseModal" v-if="isModalOpen">
       <map-view @placeChanged="setRestaurant"></map-view>
-      <div class="field column is-6">
-        <label class="label has-text-white">Meal</label>
-        <div class="control">
-          <input class="input" type="text" placeholder="Meal" v-model="meal">
-        </div>
-      </div>
-      <div class="field column is-6">
-        <label class="label has-text-white">Price</label>
-        <div class="control">
-          <input class="input" type="number" placeholder="Price" v-model="price">
-        </div>
-      </div>
       <div class="field column is-12 has-text-centered">
-        <a class="button is-warning is-fullwidth" @click="submitOrder">Submit Order</a>
+        <a class="button is-warning is-fullwidth" @click="submitOrder">Add Order</a>
       </div>
     </modal>
-    <router-view></router-view>
   </div>
 </template>
 
@@ -50,15 +33,11 @@ export default {
     return {
       tabs: ['History', 'Active'],
       isModalOpen: false,
-      price: 0,
-      meal: '',
-      restaurant: { }
+      restaurant: {}
     }
   },
   mounted() {
-    this.$store.dispatch('getOrders')
-    this.$store.dispatch('getUserData')
-    this.$store.dispatch('getRestaurants') 
+    this.$store.dispatch('watchData')
   },
   computed: {
       user() {
@@ -66,22 +45,33 @@ export default {
       },
       order() {
         return  {
-          meal: this.meal,
+          meals: [],
+          status: 'Ordered',
           restaurant: this.restaurant.id,
-          price: this.price
+          by: this.user.email
         }
       },
-      allOrders() {
-        return this.$store.getters.allOrders
-      }
   },
   methods: {
     submitOrder() {
-      this.$store.dispatch('submitOrder',  { order: this.order, restaurant: this.restaurant }) 
+      this.$store.dispatch('submitOrder',  { order: this.order, restaurant: this.restaurant })
+      this.handleCloseModal()
     },
-    setRestaurant({ formatted_address: address, id, name, international_phone_number: number }) {
-      this.restaurant = { address, id, name, number } 
-    }
+    setRestaurant({ photos, formatted_address: address, id, name, international_phone_number: number }) {
+      let image = ''
+      if (photos.length) {
+        image = photos[0].getUrl({ maxWidth: 248 })
+      }
+      this.restaurant = { image, address, id, name, number }
+    },
+    handleCloseModal() {
+      this.isModalOpen = false
+      this.resetOrder()
+    },
+    resetOrder() {
+      this.restaurant = {}
+    },
+
   }
 }
 </script>
